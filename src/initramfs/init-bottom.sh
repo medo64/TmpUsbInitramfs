d8d0e1fb778a555f4e58baa65f85e4808f58e5d8
Josip Medved <jmedved@jmedved.com>
2022-01-18 00:12:31 -0800

    Script continues if umount fails

--- src/initramfs/init-bottom.sh
+++ src/initramfs/init-bottom.sh
@@ -14,7 +14,7 @@ esac
 
 
 if [ -e "/tmpusb" ]; then
-    echo "Unmounting /tmpusb..."
-    umount /tmpusb
-    rmdir /tmpusb
+    echo "Unmounting /tmpusb"
+    umount /tmpusb 2>/dev/null || sleep 1 && umount /tmpusb 2>/dev/null || true  # give it a two tries
+    rmdir /tmpusb 2>/dev/null || true                                            # continue even if we cannot remove directory
 fi
