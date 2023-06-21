30b459d1f256cb0c14f5152e82770b26fecd9d7d
Josip Medved <jmedved@jmedved.com>
2023-06-20 18:16:00 -0700

    Reworked umounting of tmpusb to retry once more

--- src/initramfs/init-bottom.sh
+++ src/initramfs/init-bottom.sh
@@ -15,6 +15,11 @@ esac
 
 if [ -e "/tmpusb" ]; then
     echo "Unmounting /tmpusb"
-    umount /tmpusb 2>/dev/null || ( sleep 1 && umount /tmpusb 2>/dev/null || true )  # give it a two tries
-    rmdir /tmpusb 2>/dev/null || true                                                # continue even if we cannot remove directory
+    for WAIT in 1 1 2; do                                                       # give it a few tries
+        umount /tmpusb && break
+        sleep $WAIT
+    done
+
+    echo "Removing /tmpusb"
+    rmdir /tmpusb 2>/dev/null || true                                           # continue even if we cannot remove directory
 fi
